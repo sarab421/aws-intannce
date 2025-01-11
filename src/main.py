0bb1ff793fc6453b00e91c95544817aa249a61e7
Sarab Dar <darsarab421@gmail.com>
2025-01-11 13:45:28 +0500

    logging added

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
+import logging
 
 app = FastAPI()
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 @app.get("/")
 def read_root():
+    logger.info("Request on root")
     return {"message": "Hello, World!"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
+    logger.info(f"Request on id {item_id} with query {q}")
     return {"item_id": item_id, "q": q}
